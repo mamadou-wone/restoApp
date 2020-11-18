9c5d11ed131308a1e12a0704af67e88d25d889b1
Mamadou WONE <mamadouwone12345@gmail.com>
2020-11-18 17:21:31 +0000

    18/11/2020

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:restoapp/providers/app_provider.dart';
+import 'package:restoapp/screens/splash.dart';
+import 'util/const.dart';
+
+void main() async {
+  WidgetsFlutterBinding();
+  await Firebase.initializeApp();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AppProvider()),
+      ],
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AppProvider>(
+      builder: (BuildContext context, AppProvider appProvider, Widget child) {
+        return MaterialApp(
+          key: appProvider.key,
+          debugShowCheckedModeBanner: false,
+          navigatorKey: appProvider.navigatorKey,
+          title: Constants.appName,
+          theme: appProvider.theme,
+          darkTheme: Constants.darkTheme,
+          home: SplashScreen(),
+        );
+      },
+    );
+  }
+}
